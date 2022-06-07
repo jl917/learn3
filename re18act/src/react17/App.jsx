e05522cb85679b2e2a7a6d7b3ae006814daae49d
JuLong <julong@wemakeprice.com>
2022-06-07 12:12:24 +0900

    r17dom ㅊㅜ가

---
+++ re18act/src/react17/App.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useLayoutEffect } from 'react17';
+
+function App () {
+  return null;
+}
+
+export default App;
---
+++ re18act/src/react17/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react17'
+import reactDom from 'react-dom17'
+import App from './App';
+
+reactDom.render(
+  <App />,
+  document.getElementById('root'),
+)
